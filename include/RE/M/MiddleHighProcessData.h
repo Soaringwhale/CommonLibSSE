fd763ed455178e5fdbd4e09efc463de9e726583c
Refrain <69113717+Soaringwhale@users.noreply.github.com>
2025-07-12 20:24:52 +0300

    Update MiddleHighProcessData.h
    AIProcess::StartStaggerTimer_140682220

--- include/RE/M/MiddleHighProcessData.h
+++ include/RE/M/MiddleHighProcessData.h
@@ -208,7 +208,7 @@ namespace RE
 		float                                          deferredKillTimer;         // 2D4
 		float                                          killMoveTimer;             // 2D8
 		float                                          unk2DC;                    // 2DC
-		std::uint32_t                                  unk2E0;                    // 2E0
+		float                                          StaggerTimer;              // 2E0
 		std::uint32_t                                  unk2E4;                    // 2E4
 		std::uint32_t                                  currentFurnitureMarkerID;  // 2E8
 		std::uint32_t                                  unk2EC;                    // 2EC
