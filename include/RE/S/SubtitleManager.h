95f8d7edc666dd55f9b8abdf30655bea55036b68
fenix31415 <19803311+fenix31415@users.noreply.github.com>
2025-10-02 18:20:27 +0000

    maintenance

--- include/RE/S/SubtitleManager.h
+++ include/RE/S/SubtitleManager.h
@@ -27,11 +27,11 @@ namespace RE
 			return *singleton;
 		}
 
-		void ShowSubtitles (RE::TESObjectREFR *speaker, const char *text, bool alwaysDisplay)
+		void ShowSubtitles(RE::TESObjectREFR* speaker, const char* text, bool alwaysDisplay)
 		{
 			using func_t = decltype(&SubtitleManager ::ShowSubtitles);
-			REL::Relocation<func_t> func { REL::ID(51753) };
-			func (this, speaker, text, alwaysDisplay);
+			REL::Relocation<func_t> func{ REL::ID(51753) };
+			func(this, speaker, text, alwaysDisplay);
 		}
 
 		void KillSubtitles()
